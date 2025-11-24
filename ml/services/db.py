7347115f2b0d6248af113f8c53c14f243c479171
ariyalex <ariya17duta@gmail.com>
2025-11-24 18:14:01 +0700

    feat: update Docker Compose configuration and add database migration scripts

--- ml/services/db.py
+++ ml/services/db.py
@@ -6,16 +6,16 @@ load_dotenv(dotenv_path=".env")
 
 DB_HOST = os.getenv("PGHOST", "db")
 DB_PORT = os.getenv("PGPORT", "5432")
-DB_NAME = os.getenv("PGUSER", "capstone")
-DB_USER = os.getenv("PGDATABASE", "ai_learning_insight")
+DB_USER = os.getenv("PGUSER", "capstone")
+DB_NAME = os.getenv("PGDATABASE", "ai_learning_insight")
 DB_PASS = os.getenv("PGPASSWORD", "admin123")
 
 def get_connection():
     conn = psycopg2.connect(
         host=DB_HOST,
         port=DB_PORT,
-        dbname=DB_NAME,
         user=DB_USER,
+        dbname=DB_NAME,
         password=DB_PASS
     )
     return conn
